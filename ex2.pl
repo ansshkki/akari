e318c80cb404a31ef7f996e9c1a81881d9450efa
Anas Shakaki <ansshkki@gmail.com>
2022-06-14 20:38:53 +0300

    Add example 2

---
+++ ex2.pl
@@ -0,0 +1,22 @@
+size(8, 8).
+
+wall(6,2).
+wall(7,6).
+wall(7,7).
+wall(8,3).
+wall(4,1).
+wall(4,5).
+wall(5,4).
+wall(5,8).
+wall(1,6).
+wall(2,2).
+wall(2,3).
+wall(3,7).
+
+wall_num(5,8,0).
+wall_num(6,2,2).
+wall_num(7,6,1).
+wall_num(1,6,1).
+wall_num(2,2,3).
+wall_num(3,7,0).
+wall_num(5,4,4).
