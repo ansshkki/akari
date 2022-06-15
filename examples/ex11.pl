d9b296940a6f289cb6642612f0a74ede2c81f0f5
Anas Shakaki <ansshkki@gmail.com>
2022-06-15 23:03:17 +0300

    Add example

---
+++ examples/ex11.pl
@@ -0,0 +1,30 @@
+size(7, 7).
+
+wall(1, 2).
+wall(1, 3).
+wall(2, 5).
+wall(2, 7).
+wall(3, 2).
+wall(3, 7).
+wall(5, 1).
+wall(5, 6).
+wall(6, 1).
+wall(6, 3).
+wall(7, 5).
+wall(7, 6).
+
+wall_num(2, 5, 3).
+wall_num(3, 2, 0).
+wall_num(5, 1, 2).
+wall_num(5, 6, 0).
+wall_num(6, 3, 2).
+
+light(1, 5).
+light(2, 3).
+light(2, 6).
+light(3, 5).
+light(4, 1).
+light(5, 2).
+light(6, 4).
+light(7, 3).
+light(7, 7).
---
+++ examples/ex12.pl
@@ -0,0 +1,20 @@
+size(7, 7).
+
+wall(1, 2).
+wall(1, 3).
+wall(2, 5).
+wall(2, 7).
+wall(3, 2).
+wall(3, 7).
+wall(5, 1).
+wall(5, 6).
+wall(6, 1).
+wall(6, 3).
+wall(7, 5).
+wall(7, 6).
+
+wall_num(2, 5, 3).
+wall_num(3, 2, 0).
+wall_num(5, 1, 2).
+wall_num(5, 6, 0).
+wall_num(6, 3, 2).
